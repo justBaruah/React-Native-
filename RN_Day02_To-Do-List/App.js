ae75ba3e507ca77cc8e42be25a4328984457c544
Ankur Baruah <ankurbaruah04.06@gmail.com>
2024-02-22 07:02:39 +0530

    jsonfileuploaded

--- RN_Day02_To-Do-List/App.js
+++ RN_Day02_To-Do-List/App.js
@@ -1,38 +1,35 @@
 import React, { useState } from "react";
- 
 import {
+  View,
+  Text,
+  StyleSheet,
+  Keyboard,
+  TouchableOpacity,
+  TextInput,
   KeyboardAvoidingView,
   Platform,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-  Keyboard,
 } from "react-native";
- 
 import Task from "./components/task";
- 
-export default function App() {
-  const [task, setTask] = useState(""); //Keyboard Input
-  const [taskItems, setTaskItems] = useState([]); // Display Task Infos
- 
+const App = () => {
+  const [task, setTask] = useState(""); // Keyboard Input
+  const [taskItems, setTaskItems] = useState([]);
+
   const handleAddTask = () => {
-    Keyboard.dismiss();
+    Keyboard.dismiss(); // to add task
     setTaskItems([...taskItems, task]);
     setTask("");
   };
- 
   const completeTask = (index) => {
     let itemsCopy = [...taskItems];
     itemsCopy.splice(index, 1);
     setTaskItems(itemsCopy);
+    // to delete task by clicking on it
   };
- 
+
   return (
     <View style={styles.container}>
-      <View style={styles.taskswrapper}>
-        <Text style={styles.sectiontitle}>Today's Tasks</Text>
+      <View style={styles.taskwrapper}>
+        <Text style={styles.sectiontitle}>Today's Task</Text>
         <View style={styles.items}>
           {taskItems.map((item, index) => (
             <TouchableOpacity key={index} onPress={() => completeTask(index)}>
@@ -41,18 +38,17 @@ export default function App() {
           ))}
         </View>
       </View>
- 
-       <KeyboardAvoidingView
+      <KeyboardAvoidingView
         behavior={Platform.OS === "android" ? "height" : "padding"}
         style={styles.writerTaskWrapper}
       >
         <TextInput
           style={styles.input}
-          placeholder={"Write a task"}
+          placeholder={"Write your Task"}
           value={task}
           onChangeText={(text) => setTask(text)}
         />
- 
+
         <TouchableOpacity onPress={handleAddTask}>
           <View style={styles.keyboardenterbutton}>
             <Text style={styles.addText}>🙄</Text>
@@ -61,24 +57,22 @@ export default function App() {
       </KeyboardAvoidingView>
     </View>
   );
-}
- 
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#E8EAED",
   },
-  taskswrapper: {
+  taskwrapper: {
     paddingTop: 80,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
   sectiontitle: {
     fontSize: 24,
     fontWeight: "bold",
   },
-  items: {
-    marginTop: 30
-  },
+  items: { marginTop: 20 },
   writerTaskWrapper: {
     position: "absolute",
     bottom: 20,
@@ -88,42 +82,45 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
   },
   input: {
-    paddingVertical: 15,
+    paddingVertical: 10,
     maxWidth: 250,
-    paddingHorizontal: 75,
+    paddingHorizontal: 60,
     backgroundColor: "white",
-    borderRadius: 30,
     borderColor: "black",
     borderWidth: 1.5,
     width: 300,
     height: 60,
+    borderRadius: 30,
     marginBottom: 20,
-    shadowColor: "yellow", // Shadow color
-    shadowOffset: { width: -2, height: -2 },
-    shadowOpacity: 1, // Shadow opacity
-    shadowRadius: 4, // Shadow radius
+    shadowColor: "black",
+    shadowOffset: { height: -2, width: -2 },
+    shadowOpacity: 1,
+    shadowRadius: 4,
     elevation: 10,
     marginLeft: 20,
     fontSize: 17,
   },
-  writerkeyboard: {},
   keyboardenterbutton: {
     width: 60,
     height: 60,
-    backgroundColor: "white",
     borderRadius: 60,
     borderWidth: 1.5,
     justifyContent: "center",
     alignItems: "center",
     borderColor: "black",
     marginBottom: 20,
-    shadowColor: "yellow", // Shadow color
-    shadowOffset: { width: 2, height: 2 }, // Shadow offset
-    shadowOpacity: 0.75, // Shadow opacity
-    shadowRadius: 4, // Shadow radius
+    shadowOffset: { height: -2, width: -2 },
+    shadowOpacity: 1,
+    shadowRadius: 4,
     elevation: 10,
+    marginRight: 20,
+    backgroundColor: "white",
   },
-  addText: {
-    fontSize: 30,
+  addText: { fontSize: 30 },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });
+
+export default App;
--- RN_Day02_To-Do-List/components/task.js
+++ RN_Day02_To-Do-List/components/task.js
@@ -1,61 +1,65 @@
-import React from "react";
+
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
- 
+
 const Task = (props) => {
   return (
     <View style={styles.taskbox}>
       <View style={styles.itemleft}>
         <TouchableOpacity style={styles.square}></TouchableOpacity>
-        <Text style={styles.texter}>{props.text}</Text>
+        <Text style={styles.tasktext}>{props.text}</Text>
       </View>
-      <View><TouchableOpacity style={styles.circular}>
-      {/* <View style={styles.circular}></View> */}
-      </TouchableOpacity>
+      
+        <View style={styles.circular}>
+          {
+            //Codes
+          }
+        </View>
       </View>
-    </View>
+   
   );
 };
- 
+
 const styles = StyleSheet.create({
   taskbox: {
-    backgroundColor: "#FFF",
+    backgroundColor: "#FFFFFF",
     borderRadius: 10,
-    flexDirection: 'row',
-    padding:18,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-    shadowColor: 'orange', // Shadow color
-    shadowOffset: { width: 2, height: 2 }, // Shadow offset
-    shadowOpacity: 0.75, // Shadow opacity
-    shadowRadius: 4, // Shadow radius
-    elevation: 10
+    flexDirection: "row",
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 15,
+    shadowColor: "tomato",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.75,
+    shadowRadius: 4,
+    elevation: 10,
   },
   itemleft: {
     flexDirection: 'row',
     alignItems: 'center',
-    flexwrap: 'wrap' },
+    flexWrap: 'wrap',
+  },
   square: {
     width: 24,
     height: 24,
     backgroundColor: "orange",
     opacity: 0.5,
-    borderRadius: 2,
-    marginRight: 15
+    borderRadius: 5,
+    marginRight: 15,
   },
-  texter: {
+  tasktext: {
     fontWeight: "bold",
     fontSize: 15,
-    maxWidth: '80%',
+    maxWidth: "100%",
   },
+
   circular: {
     borderColor: "black",
+    borderRadius: 20,
     height: 18,
     width: 18,
     borderWidth: 2,
-    borderRadius: 20
   },
 });
- 
+
 export default Task;
- 
